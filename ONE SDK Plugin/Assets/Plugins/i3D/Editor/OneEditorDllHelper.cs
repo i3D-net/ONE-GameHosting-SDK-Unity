61b1a2256d716d4fb8ee2760b3a16e279c4f9b04
Vadim Vorotilin <vadim.vorotilin@gmail.com>
2020-12-10 20:10:43 +0200

    DLL helper for Editor

---
+++ ONE SDK Plugin/Assets/Plugins/i3D/Editor/OneEditorDllHelper.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.IO;
+using UnityEditor;
+
+namespace i3D
+{
+    /// <summary>
+    /// Helper class that adds the plugin folder to PATH so Unity Editor can find the DLL.
+    /// </summary>
+    [InitializeOnLoad]
+    public class OneDllHelper
+    {
+        static OneDllHelper()
+        {
+            string currentPath = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process);
+            string dllPath = Environment.CurrentDirectory + Path.DirectorySeparatorChar +
+                             "Assets" + Path.DirectorySeparatorChar +
+                             "Plugins" + Path.DirectorySeparatorChar +
+                             "i3D" + Path.DirectorySeparatorChar +
+                             "Windows";
+
+            if(currentPath == null || !currentPath.Contains(dllPath))
+            {
+                Environment.SetEnvironmentVariable("PATH",
+                                                   (currentPath ?? "") +
+                                                   Path.PathSeparator +
+                                                   dllPath,
+                                                   EnvironmentVariableTarget.Process);
+            }
+        }
+    }
+}
+#endif

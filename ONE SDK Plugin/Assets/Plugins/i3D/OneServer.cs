1e9749379f8fbca33ee622c734fdc7f5415990f4
Vadim Vorotilin <vadim.vorotilin@gmail.com>
2020-12-16 12:18:51 +0200

    All the server callbacks are now static (in order to aviod broken instance-method pointers)

--- ONE SDK Plugin/Assets/Plugins/i3D/OneServer.cs
+++ ONE SDK Plugin/Assets/Plugins/i3D/OneServer.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using i3D.Exceptions;
+using UnityEngine;
 
 namespace i3D
 {
     public partial class OneServer : IDisposable
     {
+        private static readonly Dictionary<IntPtr, OneServer> Servers = new Dictionary<IntPtr, OneServer>();
+
         public event Action<int> SoftStopReceived;
         public event Action<OneArray> AllocatedReceived;
         public event Action<OneArray> MetadataReceived;
@@ -43,21 +47,26 @@ namespace i3D
 
             int code = one_server_create(LogCallback, out _ptr);
             OneErrorValidator.Validate(code);
+            
+            lock (Servers)
+            {
+                Servers.Add(_ptr, this);
+            }
 
-            code = one_server_set_soft_stop_callback(_ptr, SoftStopCallback, IntPtr.Zero);
+            code = one_server_set_soft_stop_callback(_ptr, SoftStopCallback, _ptr);
             OneErrorValidator.Validate(code);
 
-            code = one_server_set_allocated_callback(_ptr, AllocatedCallback, IntPtr.Zero);
+            code = one_server_set_allocated_callback(_ptr, AllocatedCallback, _ptr);
             OneErrorValidator.Validate(code);
 
-            code = one_server_set_metadata_callback(_ptr, MetadataCallback, IntPtr.Zero);
+            code = one_server_set_metadata_callback(_ptr, MetadataCallback, _ptr);
             OneErrorValidator.Validate(code);
-
-            code = one_server_set_host_information_callback(_ptr, HostInformationCallback, IntPtr.Zero);
+            
+            code = one_server_set_host_information_callback(_ptr, HostInformationCallback, _ptr);
             OneErrorValidator.Validate(code);
-
+            
             code = one_server_set_application_instance_information_callback(
-                _ptr, ApplicationInstanceInformationCallback, IntPtr.Zero);
+                _ptr, ApplicationInstanceInformationCallback, _ptr);
             OneErrorValidator.Validate(code);
 
             code = one_server_listen(_ptr, port);
@@ -99,47 +108,79 @@ namespace i3D
             OneErrorValidator.Validate(code);
         }
 
-        private void LogCallback(int level, string log)
+        private int a = 0;
+
+        private static void LogCallback(int level, string log)
         {
-            if (_logCallback == null)
-                return;
-            
-            if (!Enum.IsDefined(typeof(OneLogLevel), level))
-                throw new OneInvalidLogLevelException(level);
-
-            var logLevel = (OneLogLevel) level;
-
-            _logCallback(logLevel, log);
+            Debug.LogFormat("{0}: {1}", level, log);
+            /// Debug.LogFormat("{0}: {1}", a, level);
+            // Debug.LogFormat("{0}: {1}", a, log);
+            // Debug.LogFormat("{0}: {1}", a, _logCallback);
+            // ++a;
+            // if (_logCallback == null)
+            //     return;
+            //
+            // if (!Enum.IsDefined(typeof(OneLogLevel), level))
+            //     throw new OneInvalidLogLevelException(level);
+            //
+            // var logLevel = (OneLogLevel) level;
+            //
+            // _logCallback(logLevel, log);
         }
 
-        private void SoftStopCallback(IntPtr data, int timeout)
+        private static void SoftStopCallback(IntPtr data, int timeout)
         {
-            if (SoftStopReceived != null)
-                SoftStopReceived(timeout);
+            if (!Servers.ContainsKey(data))
+                throw new InvalidOperationException("Cannot find OneServer instance");
+
+            var server = Servers[data];
+
+            if (server.SoftStopReceived != null)
+                server.SoftStopReceived(timeout);
         }
 
-        private void AllocatedCallback(IntPtr data, IntPtr array)
+        private static void AllocatedCallback(IntPtr data, IntPtr array)
         {
-            if (AllocatedReceived != null)
-                AllocatedReceived(new OneArray(array));
+            if (!Servers.ContainsKey(data))
+                throw new InvalidOperationException("Cannot find OneServer instance");
+
+            var server = Servers[data];
+
+            if (server.AllocatedReceived != null)
+                server.AllocatedReceived(new OneArray(array));
         }
 
-        private void MetadataCallback(IntPtr data, IntPtr array)
+        private static void MetadataCallback(IntPtr data, IntPtr array)
         {
-            if (MetadataReceived != null)
-                MetadataReceived(new OneArray(array));
+            if (!Servers.ContainsKey(data))
+                throw new InvalidOperationException("Cannot find OneServer instance");
+
+            var server = Servers[data];
+
+            if (server.MetadataReceived != null)
+                server.MetadataReceived(new OneArray(array));
         }
 
-        private void HostInformationCallback(IntPtr data, IntPtr obj)
+        private static void HostInformationCallback(IntPtr data, IntPtr obj)
         {
-            if (HostInformationReceived != null)
-                HostInformationReceived(new OneObject(obj));
+            if (!Servers.ContainsKey(data))
+                throw new InvalidOperationException("Cannot find OneServer instance");
+
+            var server = Servers[data];
+
+            if (server.HostInformationReceived != null)
+                server.HostInformationReceived(new OneObject(obj));
         }
 
-        private void ApplicationInstanceInformationCallback(IntPtr data, IntPtr obj)
+        private static void ApplicationInstanceInformationCallback(IntPtr data, IntPtr obj)
         {
-            if (ApplicationInstanceInformationReceived != null)
-                ApplicationInstanceInformationReceived(new OneObject(obj));
+            if (!Servers.ContainsKey(data))
+                throw new InvalidOperationException("Cannot find OneServer instance");
+
+            var server = Servers[data];
+
+            if (server.ApplicationInstanceInformationReceived != null)
+                server.ApplicationInstanceInformationReceived(new OneObject(obj));
         }
 
         public void Dispose()
